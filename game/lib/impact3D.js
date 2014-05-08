a1688837c43dd69ae30c13582507a11566bb091b
Toan Nguyen <toan.nguyen@2359media.com.vn>
2014-05-08 14:30:56 +0700

    try loading character into map

--- game/lib/impact3D.js
+++ game/lib/impact3D.js
@@ -1,8 +1,35 @@
-(function(window){ "use strict";
-  impact3D = {};
-  impact3D.models = impact3D.models || {};
-  impact3D.entities = impact3D.entities || {};
-  impact3D.player = impact3D.player || {};
-  impact3D.scene = null;
-  impact3D.updateCount = 0;
-});
+
+impact3D = {};
+impact3D.models = impact3D.models || {};
+impact3D.entities = impact3D.entities || {};
+impact3D.player = impact3D.player || {};
+impact3D.scene = null;
+impact3D.updateCount = 0;
+
+impact3D.init = function(scene) {
+  console.log(" *** impact3D Init *** ");
+  this.scene = scene;
+  var newObject;
+
+  for (var i = 0; i < ig.game.entities.length; i++) {
+    var entity = ig.game.entities[i];
+
+    if (this.models[entity.type]) {
+      newObject = new THREE.Mesh( this.models[entity.type], new THREE.MeshBasicMaterial({color:0x00ff00, wireframe:true}) );
+    } else {
+      newObject = new THREE.Mesh( new THREE.CubeGeometry(8,8,8), new THREE.MeshPhongMaterial({color:0xff00ff}) );
+    }
+
+    if (entity.type == 1) {
+      this.player = newObject;
+    }
+
+    this.entities[entity.id] = newObject;
+
+    this.scene.add(newObject);
+  }
+}
+
+impact3D.addModel = function(geometry, type) {
+  this.models[type] = geometry;
+}
